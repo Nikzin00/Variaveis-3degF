84293ace461e14607d86db529a15c49e30ea18d4
Nikzin00 <126522986+Nikzin00@users.noreply.github.com>
2024-09-02 22:43:35 +0000

    2-09-24

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const anoNascimento = 2007;
 let anoAtual = 2024;
 const nomeCompleto = "Nikolas Matheus Viana de Quevedo";
 
-console.log("Meu nome é " + nomeCompleto + "Nascido no ano de " + anoNascimento + "e no ano atual " + anoAtual + "completo " + idade + "anos")
+console.log("Meu nome é " + nomeCompleto + "Nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos")
 
 anoAtual = anoAtual + 1;
 
@@ -11,3 +11,21 @@ console.log(anoAtual);
 
 idade = idade + 1;
 console.log(idade);
+
+
+//Agência de viagens Los Angeles, Nova York, Alemanha, Londres, Tokio
+const listaDeDestinos = new Array(
+  "Los Angeles",
+  "Nova York",
+  "Alemanha",
+  "Londres",
+  "Tokio"
+);
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[3]);
+listaDeDestinos.push("Coronel Vivida");
+listaDeDestinos.push("São Paulo");
+listaDeDestinos.push("Gramados");
+listaDeDestinos.push("Berlin");
+console.log(listaDeDestinos);
